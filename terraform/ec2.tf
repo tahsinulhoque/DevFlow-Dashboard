49990df3458dd1f186850aa5e0866f988eff57e3
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-04-26 00:34:44 +0600

    add ec2 setup with terraform

---
+++ terraform/ec2.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "devflow_sg" {
+  name        = "Devflow-sg"
+  description = "Allow HTTP & SSH"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 🔥 Dynamic AMI (correct way)
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "DevFlow_ec2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.devflow_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "DevFlow-server"
+  }
+}

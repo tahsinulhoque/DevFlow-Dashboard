58458876b93584c1c7b8edeff0ea917d6723a2a5
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-04-28 00:36:59 +0600

    Change Ec2 code

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,18 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_security_group" "devflow_sg" {
   name        = "Devflow-sg"
   description = "Allow HTTP & SSH"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -25,29 +36,16 @@ resource "aws_security_group" "devflow_sg" {
   }
 }
 
-# 🔥 Dynamic AMI (correct way)
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
 resource "aws_instance" "DevFlow_ec2" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
-  subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.devflow_sg.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.devflow_sg.id]
   associate_public_ip_address = true
 
   key_name = "devflow-key"
 
-  # 🔥 IMPORTANT FIX
   depends_on = [
     aws_internet_gateway.igw
   ]
